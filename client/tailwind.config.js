bf079f7fa2757504ecbf0c4fc662b2b24297c30c
Jay Mhatre <jay17mhatre@gmail.com>
2024-07-20 17:22:48 +0530

    deleted tailwind css file by fault

---
+++ client/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"node_modules/flowbite-react/lib/esm/**/*.js",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				robo: ['"Roboto Slab"'],
+			},
+		},
+		screens: {
+			sm: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+		},
+	},
+	plugins: [require("flowbite/plugin"), require("tailwindcss-animated")],
+};
